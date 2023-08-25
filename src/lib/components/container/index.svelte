6d33c700a80e545039b859083fd2f7c4a5caf756
Nico Plyley <nico@phoenixtechlab.com>
2023-08-24 19:03:10 -0700

    :sparkles: Created container component

---
+++ src/lib/components/container/index.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	let className;
+
+	// noinspection JSUnusedGlobalSymbols
+	export { className as class };
+</script>
+
+<div class="container mx-auto px-2 {className}">
+	<slot />
+</div>
