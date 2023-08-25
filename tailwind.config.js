800c57da4dd3eb31b736e7567ddbb15c547b1a27
Nico Plyley <nico@phoenixtechlab.com>
2023-08-24 19:08:24 -0700

    :wrench: Added link for creating color schemes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+
+// * Create color scheme with link below
+// @ https://uicolors.app/create
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
